85c337c727e6503f703966fc30940e2f37d83207
unknown <yc2478@cornell.edu>
2021-01-08 14:40:34 -0500

    animation

--- Player.gd
+++ Player.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 const GRAVITY = 4.2
 const JUMP_SPEED = -150
 var velocity = Vector2.ZERO
+onready var animation = $AnimatedSprite
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,5 +16,11 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_SPEED
-
+		
+	if velocity.y<0:
+		animation.play("jumpUp")
+	elif velocity.y>0 and is_on_floor():
+		animation.play("sheepMove")
+	else:
+		animation.play("fallDown")
 	move_and_slide(velocity, Vector2.UP)
