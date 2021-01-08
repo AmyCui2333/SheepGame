7f6b7d87b500fea93d7c31f054f34539057d7b9c
unknown <yc2478@cornell.edu>
2021-01-08 12:27:29 -0500

    jump floor

--- Player.gd
+++ Player.gd
@@ -13,7 +13,7 @@ func _ready():
 func _process(delta):
 	velocity.y += GRAVITY
 	
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_SPEED
 
-	move_and_slide(velocity)
+	move_and_slide(velocity, Vector2.UP)
